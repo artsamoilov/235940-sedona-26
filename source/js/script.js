585c508dc7efed747f177c389f06c93f821d2972
Arthur Samoilov <artsamoilov@gmail.com>
2022-08-28 01:40:15 +0300

    add video existing check

--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,6 @@
 const mainNav = document.querySelector('.navigation');
 const mainNavToggle = document.querySelector('.navigation__toggle');
 const mainNavList = document.querySelector('.navigation__list');
-
 const videoContainer = document.querySelector('.presentation__content');
 const videoPlayButton = document.querySelector('.presentation__play-button');
 const video = document.querySelector('.presentation__video');
@@ -13,7 +12,7 @@ mainNavToggle.addEventListener('click', () => {
   mainNavToggle.classList.toggle('navigation__toggle--opened');
 });
 
-videoContainer.addEventListener('click', () => {
+videoContainer && videoContainer.addEventListener('click', () => {
   videoPlayButton.classList.contains('presentation__play-button--pause') ? video.pause() : video.play();
   videoPlayButton.classList.toggle('presentation__play-button--pause');
 });
